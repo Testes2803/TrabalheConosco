196e1f3ebfe2d7a5a4399d67d4f4a0bf1fd61fdd
Natasha Mikaela <natashamendes@bne-empregos.com.br>
2019-12-03 13:30:53 -0300

    Limpeza dos comentarios

--- Js/Index.js
+++ Js/Index.js
@@ -12,30 +12,3 @@ function trocaClasse() {
         document.getElementsByClassName("logo__image").addEventListener('click', changeClass);
     }
 }
-
-function more() {
-    $('.primeiro').css('display', 'none');
-    //$('.primeiro').css('display', 'none');
-    //$('.primeiro').css('transition', 'height .3s ease');
-
-    $('.segundo').css('display', 'flex');
-}
-
-function back() {
-    $('.primeiro').css('display', 'flex');
-    $('.segundo').css('display', 'none');
-}
-
-/*$('.jobs').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });*/
-
-/*$(document).ready(function () {
-    document.getElementsByClassName('jobs')[0].slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-    });
-});*/
